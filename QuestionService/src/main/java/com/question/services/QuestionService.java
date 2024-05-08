ebee5b4b0a93211fa534241542468a73c0e7a6ae
blovanshi <brajesh.lovanshi@mplifytech.com>
2024-05-08 19:42:02 +0530

    application property

--- QuestionService/src/main/java/com/question/services/QuestionService.java
+++ QuestionService/src/main/java/com/question/services/QuestionService.java
@@ -1,4 +1,13 @@
 package com.question.services;
 
-public class QuestionService {
+import com.question.entities.Question;
+
+import java.util.List;
+
+public interface QuestionService {
+
+    Question add(Question question);
+    List<Question> get();
+    Question get(Long id);
+    List<Question> getByQuizId(Long id);
 }
