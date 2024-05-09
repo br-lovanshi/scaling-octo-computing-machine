4bdb101032da81ace22d1150497c8c7af6cd8d41
blovanshi <brajesh.lovanshi@mplifytech.com>
2024-05-09 19:37:10 +0530

    question entity created

--- QuizeService/src/main/java/com/quize/entities/Question.java
+++ QuizeService/src/main/java/com/quize/entities/Question.java
@@ -1,4 +1,18 @@
 package com.quize.entities;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
 public class Question {
+
+    private long questionId;
+    private String question;
+    private Long quizId;
+
 }
