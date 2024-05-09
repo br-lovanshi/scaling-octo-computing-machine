376cea296e97567e8a2f6199a853c62bf776365b
blovanshi <brajesh.lovanshi@mplifytech.com>
2024-05-09 19:37:33 +0530

    question client created

--- QuizeService/src/main/java/com/quize/services/QuestionClient.java
+++ QuizeService/src/main/java/com/quize/services/QuestionClient.java
@@ -1,4 +1,15 @@
 package com.quize.services;
 
+import com.quize.entities.Question;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
+
+@FeignClient(name = "question-service", url = "http://localhost:9090")
 public interface QuestionClient {
+
+    @GetMapping("/questions/{quizId}")
+    List<Question> getQustionOfQuiz(@PathVariable Long quizId);
 }
