81ed3e78c93da63d41b17f91616a458edf9f3319
blovanshi <brajesh.lovanshi@mplifytech.com>
2024-06-11 19:23:21 +0530

    update endpoints

--- mailSender/src/main/java/com/mail/mailSender/controller/MailJobController.java
+++ mailSender/src/main/java/com/mail/mailSender/controller/MailJobController.java
@@ -2,6 +2,7 @@ package com.mail.mailSender.controller;
 
 import com.mail.mailSender.dto.mailJob.GetAllJobDTO;
 import com.mail.mailSender.dto.mailJob.MailJobCreateReqeust;
+import com.mail.mailSender.dto.mailJob.MailJobResponseDTO;
 import com.mail.mailSender.model.MailJob;
 import com.mail.mailSender.response.SuccessResponse;
 import com.mail.mailSender.response.SuccessResponseHandler;
@@ -18,7 +19,7 @@ import java.util.List;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api/mail-job")
 public class MailJobController {
 
     @Autowired
@@ -26,25 +27,25 @@ public class MailJobController {
 
     private static final Logger logger = LoggerFactory.getLogger(MailJobController.class);
 
-    @PostMapping("/mail-job")
+    @PostMapping()
     public ResponseEntity<SuccessResponse<?>> addMailJob(@RequestBody @Valid MailJobCreateReqeust mailJobCreateReqeust) throws Exception {
 
 
         MailJob mailJob = mailJobInterface.storeMailJob(mailJobCreateReqeust);
 
-        MailJobCreateReqeust mailJobCreateReqeust1 = new MailJobCreateReqeust(mailJob);
+        MailJobResponseDTO mailJobResponseDTO = new MailJobResponseDTO(mailJob);
 
         SuccessResponse<?> successResponse = SuccessResponseHandler.buildSuccessResponse(
                 HttpStatus.CREATED,
                 "Mail job created successfully.",
-                Optional.of(mailJobCreateReqeust1)
+                Optional.of(mailJobResponseDTO)
         );
 
         return new ResponseEntity<>(successResponse, HttpStatus.CREATED);
     }
 
 
-    @GetMapping("/mail-jobs")
+    @GetMapping()
     public ResponseEntity<SuccessResponse<?>> getAllMailJobs(){
 
             List<GetAllJobDTO> mailJobs = mailJobInterface.getAllMailJob();
@@ -54,15 +55,16 @@ public class MailJobController {
 
     }
 
-    @GetMapping("/mail-job/{id}")
+    @GetMapping("/{id}")
     public ResponseEntity<SuccessResponse<?>> getMailJobById(@PathVariable Long id){
         MailJob mailJob = mailJobInterface.getMailJob(id);
-        MailJobCreateReqeust mailJobCreateReqeust = new MailJobCreateReqeust(mailJob);
-        SuccessResponse<?> successResponse = SuccessResponseHandler.buildSuccessResponse(HttpStatus.OK,"Data retrived successfully.",Optional.of(mailJobCreateReqeust));
+        MailJobResponseDTO mailJobResponseDTO = new MailJobResponseDTO(mailJob);
+
+        SuccessResponse<?> successResponse = SuccessResponseHandler.buildSuccessResponse(HttpStatus.OK,"Data retrived successfully.",Optional.of(mailJobResponseDTO));
         return new ResponseEntity<>(successResponse, HttpStatus.OK);
     }
 
-    @DeleteMapping("/mail-job/{id}")
+    @DeleteMapping("/{id}")
     public ResponseEntity<SuccessResponse<?>> deleteMailJobById(@PathVariable Long id){
         mailJobInterface.deleteMailJob(id);
         SuccessResponse<?> successResponse = SuccessResponseHandler.buildSuccessResponse(HttpStatus.NO_CONTENT, "Data Deleted successfully.",Optional.empty());
--- mailSender/src/main/java/com/mail/mailSender/controller/SMTPConfigController.java
+++ mailSender/src/main/java/com/mail/mailSender/controller/SMTPConfigController.java
@@ -1,64 +1,58 @@
 package com.mail.mailSender.controller;
 
-import com.mail.mailSender.dto.mailJob.SMTPConfig.GetAllSMTPConfigDTO;
+import com.mail.mailSender.dto.smtpConfig.GetAllSMTPConfigDTO;
 import com.mail.mailSender.model.SMTPConfig;
 import com.mail.mailSender.response.SuccessResponse;
 import com.mail.mailSender.response.SuccessResponseHandler;
 import com.mail.mailSender.service.smtp.SMTPConfigInterface;
 import com.mail.mailSender.service.smtp.SMTPConfigInterfaceImpl;
 import jakarta.validation.Valid;
-import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api/smtp-config")
 public class SMTPConfigController {
 
     @Autowired
-    SMTPConfigInterface smtpConfigInterface = new SMTPConfigInterfaceImpl();
+    SMTPConfigInterface smtpConfigService;
 
-    @PostMapping("/smtp-config")
+    @PostMapping()
     public ResponseEntity<SuccessResponse<?>> storeSMTP(@RequestBody @Valid  SMTPConfig smtpConfig) throws Exception {
-        smtpConfigInterface.storeSMTPConfig(smtpConfig);
+        smtpConfigService.storeSMTPConfig(smtpConfig);
         SuccessResponse<?> successResponse = SuccessResponseHandler.buildSuccessResponse(HttpStatus.CREATED,"SMTP configuration stored successfully.", Optional.empty());
         return new ResponseEntity<>(successResponse,HttpStatus.CREATED);
     }
 
-    @GetMapping("/smtp-config/{id}")
+    @GetMapping("/{id}")
     public ResponseEntity<SuccessResponse<?>> getSMTPConfigById(@PathVariable Long id) throws Exception{
 
-            SMTPConfig smtpConfig = smtpConfigInterface.getSMTPConfig(id);
+            SMTPConfig smtpConfig = smtpConfigService.getSMTPConfig(id);
             SuccessResponse<SMTPConfig> successResponse = SuccessResponseHandler.buildSuccessResponse(HttpStatus.OK,"Data retrieved successfully.",Optional.of(smtpConfig));
             return new ResponseEntity<>(successResponse,HttpStatus.OK);
     }
 
-    @GetMapping("/smtp-config")
+    @GetMapping("")
     public ResponseEntity<SuccessResponse<?>> getAllSMTPConfig() throws Exception{
 
-        List<GetAllSMTPConfigDTO> smtpConfigList = smtpConfigInterface.getAllSMTPConfig();
+        List<GetAllSMTPConfigDTO> smtpConfigList = smtpConfigService.getAllSMTPConfig();
         SuccessResponse<?> successResponse = SuccessResponseHandler.buildSuccessResponse(HttpStatus.OK,"Data retrieved successfully.",Optional.of(smtpConfigList));
         return new ResponseEntity<>(successResponse,HttpStatus.OK);
 
     }
 
-    @DeleteMapping("/smtp-config/{id}")
-    public ResponseEntity<?> deleteSMTPConfigById(@PathVariable Long id){
-        try{
-            smtpConfigInterface.deleteSmtpConfig(id);
-            return new ResponseEntity<>("App removed successfully.",HttpStatus.NO_CONTENT);
-        }catch (Exception e){
-            return new ResponseEntity<>(e.getMessage(),HttpStatus.NOT_FOUND);
-        }
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deleteSMTPConfigById(@PathVariable Long id) throws Exception{
+
+            smtpConfigService.deleteSmtpConfig(id);
+            SuccessResponse<?> successResponse = SuccessResponseHandler.buildSuccessResponse(HttpStatus.NO_CONTENT,"Data removed successfully.", Optional.empty());
+            return new ResponseEntity<>(successResponse,HttpStatus.NO_CONTENT);
+
     }
 
 }
